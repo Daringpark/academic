051ce97474e6dd8caf5ef16ec5d5d1b26d8cce76
Daringpark <shoostar0611@gmail.com>
2024-04-29 02:33:56 +0900

    update

--- pjt/PJT07/examplecode/weathers/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Weather(models.Model):
-    dx_txt = models.DateTimeField() # 관측 시간
-    temp = models.FloatField()      # 당시 온도 (단위 : K)
-    feels_like = models.FloatField() # 체감 온도 (단위 : K)
--- pjt/PJT07/examplecode/weathers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('temp/', views.temp, name = 'tempbydt'),
-    path('hottemp/', views.hot_weathers, name = 'hottemp'),
-]
